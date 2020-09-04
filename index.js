3dcf691e5a1306e571d87197af010c941f0a7954
Tuer Hulz <darkost12@gmail.com>
2020-09-04 15:04:37 +0300

    Works nicely

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
 const { readLine } = require('./console');
 const path = require('path');
+const arraySort = require('array-sort');
+
+
 app();
 
 function app () {
@@ -20,27 +23,37 @@ function getFiles () {
 function extractComments (paths, files) {
     const regex = /(?<=\/\/ TODO\s)(.*)/gi;
     const fields = ['author', 'date', 'text'];
+    const capacities = [10, 10, 50, 15];
     let comments = [];
     for (let i = 0; i < files.length; i++) {
         files[i] = files[i].match(regex);
         if (!files[i]) continue;
         for (let j = 0; j < files[i].length; j++) {
             let components = files[i][j].split(';');
-            let params = [];
+            let params = {};
             if (components.length == 1) {
-                params['text'] = components[0].trim();
+                params['text'] = shorten(components[0].trim(), capacities[2]);
             } else {
                 for (let j = 0; j < components.length; j++) {
                     components[j] = components[j].trim();
                     if (components[j].length == 0) continue;
-                    params[fields[j]] = components[j];
+                    params[fields[j]] = shorten(components[j], capacities[j]);
                 }
             }
-            params['path'] = path.basename(paths[i]);
+            params['path'] = shorten(path.basename(paths[i]), capacities[3]);
+            params['priority'] = getImportance(params['text']);
             comments.push(params);
         }
     }
     return comments;
+}
+
+function shorten (str, lim) {
+        return str.length > lim ? (str.substring(0, lim - 3) + '...') : str;
+}
+
+function getImportance (text) {
+    return (text.match(/!/g) || []).length;
 }
 function processCommand (command) {
     switch (command) {
